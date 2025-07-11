27f8fa8b8e2903f3ce74250057f9e6cad955098f
Alice Kondryuchaya <alisa.kondryuchaya@jetruby.com>
2025-07-11 17:29:48 +0300

    second test(not wirking)

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -19,3 +19,29 @@ test('login authorized user', async ({ browser }) => {
 
     await expect(page.locator('h1.mb-6')).toContainText('Forms');
 });
+
+test('login user with end date of period', async ({browser}) => {
+ const context = await browser.newContext();
+ const page = await context.newPage();
+
+ await page.goto('https://api.jeterp-staging.jetruby.cloud/admin/session/new');
+ await page.click('text=Log In with Google');
+
+ await page.waitForSelector('input[name="query"]');
+ await page.click('a[href="/admin/people/2a409428-b370-4890-bf12-6d91d95cd8d9"]');
+ 
+ await page.waitForSelector('a.btn.btn-primary[href*="/edit"]', { timeout: 10000 });
+ await page.click('a.btn.btn-primary[href*="/edit"]');
+
+  await page.waitForSelector('#person_actual_period_attributes_finished_at');
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate-1);
+  const dateStr = yesterday.toISOString().split('T')[0];
+  await page.fill ('#person_actual_period_attributes_finished_at', dateStr);
+  await page.click('input[type="submit"][name="commit"][value="Save"]')
+  
+  await page.waitForNavigation({ waitUntil: 'networkidle' });
+  const toast = page.locator('.alert.alert-info');
+  await expect(toast).toBeVisible({ timeout: 5000 });
+  await expect(toast).toContainText('Updated successfully.');
+});
