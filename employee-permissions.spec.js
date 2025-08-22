5919d61c9ccf851debafaffce7b11b47cbcfd5c7
Alice Kondryuchaya <alisa.kondryuchaya@jetruby.com>
2025-08-22 18:47:23 +0300

    add-first-permission-for-employee

---
+++ employee-permissions.spec.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+let sharedPage;
+
+test.describe('Permissions for employee', () => {
+  test('Authorization', async ({ browser }) => {
+    const context = await browser.newContext();
+    sharedPage = await context.newPage();
+
+    await sharedPage.goto('https://jeterp-staging.jetruby.cloud/');
+    await sharedPage.click('text=Continue with SSO');
+    await sharedPage.waitForSelector('input[name="username"]');
+    await sharedPage.fill('input[name="username"]', 'erp.test+employee@jetruby.com');
+    await sharedPage.fill('input[name="password"]', 'ERPtest123!');
+    await sharedPage.click('input[type="submit"][value="Sign In"]');
+    await sharedPage.waitForSelector('text=Forms', { timeout: 10000 });
+  });
+
+  test('Open Calendar', async () => {
+    await sharedPage.click('a[data-tooltip-content="Calendar"]');
+    await expect(sharedPage.locator('h1')).toContainText('Calendar');
+  });
+
+  test.afterAll(async () => {
+    if (sharedPage) {
+      await sharedPage.context().close();
+    }
+  });
+});
